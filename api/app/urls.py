4dd5a91ec610c8cd7cb53d00f96222278653fa6c
Łukasz Glapiak <37459582+Lukas2018@users.noreply.github.com>
2020-10-22 11:49:55 +0200

    Cleaning

--- api/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from api.views import UserView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls'))
-]
